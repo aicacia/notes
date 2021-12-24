1a5ac0f65e461c6c25db85cef87a85d1bdbbc3bb
nathanfaucett <nathanfaucett@users.noreply.github.com>
2021-12-24 19:31:45 +0000

    deploy: 5ed136474f6e1a78d290e08b1779995a8bbfea51

---
+++ service-worker.js
@@ -0,0 +1,49 @@
+const timestamp = 1640374302433;
+const build = [
+  "/notes/_app/start-390579db.js",
+  "/notes/_app/assets/start-d5b4de3e.css",
+  "/notes/_app/pages/__layout.svelte-afc317a3.js",
+  "/notes/_app/assets/pages/__layout.svelte-84705efb.css",
+  "/notes/_app/error.svelte-ef954d4d.js",
+  "/notes/_app/pages/index.svelte-14183730.js",
+  "/notes/_app/assets/pages/index.svelte-3e35cdcb.css",
+  "/notes/_app/chunks/vendor-8e921715.js",
+  "/notes/_app/assets/vendor-86f8c920.css",
+  "/notes/_app/chunks/paths-28a87002.js",
+  "/notes/_app/chunks/preload-helper-b6664b8f.js",
+  "/notes/_app/chunks/index-7a1bb196.js",
+  "/notes/_app/chunks/widget-3e963cc8.js"
+];
+const files = [
+  "/notes/favicon.png",
+  "/notes/icon.png",
+  "/notes/manifest.json",
+  "/notes/robots.txt"
+];
+const CACHE_NAME = `static-cache-v${timestamp}`;
+const FILES_TO_CACHE = [...build, ...files];
+self.addEventListener("install", (evt) => {
+  console.log("[ServiceWorker] Install");
+  evt.waitUntil(caches.open(CACHE_NAME).then((cache) => {
+    console.log("[ServiceWorker] Pre-caching offline page");
+    return cache.addAll(FILES_TO_CACHE);
+  }));
+  self.skipWaiting();
+});
+self.addEventListener("activate", (evt) => {
+  console.log("[ServiceWorker] Activate");
+  evt.waitUntil(caches.keys().then((keyList) => Promise.all(keyList.map((key) => {
+    if (key !== CACHE_NAME) {
+      console.log("[ServiceWorker] Removing old cache", key);
+      return caches.delete(key);
+    }
+  }))));
+  self.clients.claim();
+});
+self.addEventListener("fetch", (evt) => {
+  console.log("[ServiceWorker] Fetch", evt.request.url);
+  if (evt.request.mode !== "navigate") {
+    return;
+  }
+  evt.respondWith(fetch(evt.request).catch(() => caches.open(CACHE_NAME).then((cache) => cache.match("offline.html"))));
+});
